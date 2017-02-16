41c5ca47f40c08ce63f27874c152b5ee9d5e6c94
Eugene Niyitanga <eugene.niyitanga2015@gmail.com>
2017-02-16 17:26:10 +0200

    created env variables with dot_env gem to DRY up the codes

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,11 @@
 class TeamsController < ApplicationController
 
   def show
-    @team = Unirest.get("http://localhost:3000/api/v2/teams/#{params[:id]}.json").body
+    @team = Unirest.get("#{ENV['DOMAIN']}/teams/#{params[:id]}.json").body
   end
   
   def index
-    @teams = Unirest.get("http://localhost:3000/api/v2/teams.json").body
+    @teams = Unirest.get("#{ENV['DOMAIN']}/teams.json").body
   end
   
   def new 
@@ -13,23 +13,23 @@ class TeamsController < ApplicationController
   end
 
   def create
-      @team = Unirest.post("http://localhost:3000/api/v2/teams.json", :headers => {"Accept"=> "application/json"}, :parameters => {:creature => params[:creature], :name => params[:name], :state=> params[:state], :sport => params[:sport]}).body 
+      @team = Unirest.post("#{ENV['DOMAIN']}/teams.json", :headers => {"Accept"=> "application/json"}, :parameters => {:creature => params[:creature], :name => params[:name], :state=> params[:state], :sport => params[:sport]}).body 
     redirect_to "/teams/#{@team['id']}"
   end
 
   def edit 
-    @team = Unirest.get("http://localhost:3000/api/v2/teams/#{params[:id]}.json").body
+    @team = Unirest.get("#{ENV['DOMAIN']}/teams/#{params[:id]}.json").body
     
   end
 
   def update 
-    @team = Unirest.patch("http://localhost:3000/api/v2/teams.json", :headers => {"Accept"=> "application/json"}, :parameters => {:creature => params[:creature], :name => params[:name], :state=> params[:state], :sport => params[:sport]}).body
+    @team = Unirest.patch("#{ENV['DOMAIN']}/teams.json", :headers => {"Accept"=> "application/json"}, :parameters => {:creature => params[:creature], :name => params[:name], :state=> params[:state], :sport => params[:sport]}).body
     redirect_to "/teams/#{@team['id']}"
     
   end
 
   def destroy
-    Unirest.delete("http://localhost:3000/api/v2/teams/#{params[:id]}.json").body
+    Unirest.delete("#{ENV['DOMAIN']}/teams/#{params[:id]}.json").body
     redirect_to "/teams"
   end
 end
