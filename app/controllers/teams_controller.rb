2a7ec64f763824fdc533fd60af04d816509086f8
Eugene Niyitanga <eugene.niyitanga2015@gmail.com>
2017-02-16 14:41:27 +0200

    created index

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,14 +1,12 @@
 class TeamsController < ApplicationController
 
   def show
-    @team = Unirest.get("http://localhost:3000/teams/#{params[:id]}.json").body
-  #   @team = Unirest.get("http://localhost:3000/api/v1/teams/#{params[:id]}.json").body
+    # @team = Unirest.get("http://localhost:3000/teams/#{params[:id]}.json").body
+    @team = Unirest.get("http://localhost:3000/api/v2/teams/#{params[:id]}.json").body
   end
   
   def index
-    @teams = Unirest.get("http://localhost:3000/teams.json").body
-  #   @teams = Unirest.get("http://localhost:3000/api/v1/teams.json").body
-  #   render "index.html.erb"
+    @teams = Unirest.get("http://localhost:3000/api/v2/teams.json").body
   end
   
   # def create
